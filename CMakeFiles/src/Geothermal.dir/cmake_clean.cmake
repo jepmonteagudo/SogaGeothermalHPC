02666c0a278637fca4a9fc5f7b7ff7797aa8d896
shyaan <shyaansun@outlook.com>
2020-02-28 18:34:46 -0800

    finial

---
+++ CMakeFiles/src/Geothermal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src/Geothermal.dir/src/Geothermal.cc.o"
+  "src/Geothermal"
+  "src/Geothermal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src/Geothermal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
